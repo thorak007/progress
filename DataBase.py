816d9a4b4289df43ad4ac11ff857cfa56c0d610f
Taras <thorak2001@gmail.com>
2018-12-12 10:05:54 +0200

    change database

--- DataBase.py
+++ DataBase.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-class Database:
+class DataBase:
     def create_database(self):
-        conn = sqlite3.connect("ProgressDataBase.db")
+        conn = sqlite3.connect("Subjects.db")
         cursor = conn.cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS Progress(ID INTEGER
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Subjects(ID INTEGER,
                                                             Subject TEXT,
                                                             January INTEGER, 
                                                             February INTEGER, 
@@ -22,18 +22,16 @@ class Database:
         conn.commit()
 
     def input_data_in_database(self, subject):
-        conn = sqlite3.connect("ProgressDataBase.db")
+        conn = sqlite3.connect("Subjects.db")
         cursor = conn.cursor()
-        self.progress = [(subject, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)]
-
-        cursor.executemany()
-
+        self.progress = [(subject, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)]
+        cursor.executemany("INSERT INTO Subjects VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", self.progress)
         conn.commit()
 
     def select_data_from_database(self):
         a = []
-        conn = sqlite3.connect("ProgressDataBase.db")
+        conn = sqlite3.connect("Subjects.db")
         cursor = conn.cursor()
-        for row in cursor.execute("SELECT rowid, * FROM Progress"):
+        for row in cursor.execute("SELECT rowid, * FROM Subjects"):
             a.append(row)
         return a
